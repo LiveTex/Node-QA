0e24d52b2b8a866d27483b1efee598e39d104cc8
Evgeny Mikheev <eudgee@gmail.com>
2013-10-31 10:22:17 +0400

    last gloss

--- etc/index.d
+++ etc/index.d
@@ -14,9 +14,9 @@ qa/business/business.js
 qa/business/utils/utils.js
 qa/business/utils/async/async.js
 
-qa/business/io/chat-server-slave.js
+qa/business/io/io.js
 qa/business/io/i-connection.js
-qa/business/io/protocol.js
+qa/business/io/i-protocol.js
 qa/business/io/chat-server-protocol.js
 qa/business/io/chat-server-connection.js
 qa/business/io/livetex-server-protocol.js
@@ -25,16 +25,11 @@ qa/business/io/member-session.js
 qa/business/io/visitor-session.js
 
 qa/business/entity/feature.js
-qa/business/entity/group.js
-qa/business/entity/i-group-holder.js
-qa/business/entity/user.js
 qa/business/entity/member-auth-status.js
 qa/business/entity/member.js
 qa/business/entity/i-member-holder.js
 qa/business/entity/visitor.js
 qa/business/entity/i-visitor-holder.js
-qa/business/entity/chat.js
-qa/business/entity/i-chat-holder.js
 
 qa/business/entity/factory/factory.js
 
