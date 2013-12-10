f686e4241da5111db7b446f8a758bd3452dfb0e3
Sergey Kononenko <kononencheg@gmail.com>
2013-12-10 15:50:52 +0400

    a lot of changes

--- etc/index.d
+++ etc/index.d
@@ -1,12 +1,26 @@
 
 qa/qa.js
-qa/report-item.js
 
-qa/context/i-test-context.js
-qa/context/i-context-factory.js
-qa/context/test-context.js
-qa/context/context.js
+qa/assert/assert.js
 
-qa/app/app.js
-qa/app/i-application.js
-qa/app/i-application-factory.js
+qa/log/i-event-log.js
+qa/log/i-event-iterator.js
+qa/log/event-log.js
+qa/log/event-iterator.js
+qa/log/log.js
+
+qa/test/i-application.js
+qa/test/i-context-factory.js
+qa/test/i-context.js
+qa/test/context.js
+
+qa/test/events/test-event.js
+qa/test/events/test-start.js
+qa/test/events/exception.js
+qa/test/events/assertion.js
+qa/test/events/timeout.js
+qa/test/events/test-end.js
+qa/test/events/actor-completed.js
+
+qa/storage/storage.js
+qa/storage/shared/shared.js
