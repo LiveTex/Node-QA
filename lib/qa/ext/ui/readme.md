f7d4bded8d5769813ceef9cfd339cb6b2cf1cc8a
Aleksandr Mihailov <aleksandr.mi@livetex.ru>
2014-05-15 13:36:03 +0400

    mission complit

---
+++ lib/qa/ext/ui/readme.md
@@ -0,0 +1,28 @@
+Selenium
+========================
+
+###How to
+
+##Download
+
+[Download page](http://docs.seleniumhq.org/download/)
+
+Download Selenium Server and some of third party browser drivers, if needed.
+
+##Run
+
+Run selenium standalone
+
+```
+java -jar selenium-server-standalone-*.jar
+```
+
+If you use third party drivers start it.
+i.e. chromedriver
+
+```
+chmod +x ./chromedriver
+./chromedriver
+```
+
+##All should be OK =)
--- test/selenium.js
+++ test/selenium.js
@@ -1,32 +1,60 @@
 qa = require('../bin');
 async = require('node-async');
 
-function complete(data){
+function complete(data) {
   console.log(data);
 }
+
 var browser = new qa.ext.ui.Browser('localhost', 4444);
 
+var fnc = function() {
 
-var steps = async.script.sequence([
-  browser.createSession('chrome'),
-  async.console.log(),
-  browser.setUrl('http://yandex.ru'),
-  async.console.log(),
-  browser.setTimeout('implicit', 200),
-  async.console.log(),
-  browser.findElement('id', 'tab-maps'),
-  async.console.log(),
-  browser.produceElementAction('click'),
-  async.console.log(),
-  browser.findElement('id', 'text'),
-  async.console.log(),
-  browser.produceElementAction('value', 'Saint-Petersburg Savushkina'),
-  browser.produceElementAction('submit'),
-  //async.console.log(),
-  //browser.destroySession(),
-  //async.console.log(),
-]);
-steps.call(null, null, complete, complete);
+  var temp = new async.script.Value();
+
+  return async.script.sequence([
+    browser.createSession('chrome'),
+    async.console.log('create session'),
+    browser.setUrl('http://yandex.ru'),
+    async.console.log('setUrl'),
+    browser.setTimeout('implicit', 200),
+    async.console.log('setTimeout'),
+    browser.findElement('id', 'tab-maps'),
+    async.console.log('findElement'),
+    browser.produceElementAction('click'),
+    async.console.log('produceElementAction'),
+    browser.findElement('id', 'text'),
+    async.console.log('findElement'),
+    temp.assign(),
+    browser.produceElementAction('value', 'Saint-Petersburg Savushkina'),
+    async.console.log('set value'),
+    browser.produceElementAction('submit'),
+    async.console.log('produce submit'),
+    browser.getElementData('text'),
+    async.console.log('text'),
+    temp,
+    browser.getElementData('name'),
+    async.console.log('name'),
+    temp,
+    browser.getElementData('selected'),
+    async.console.log('selected'),
+    temp,
+    browser.getElementData('enabled'),
+    async.console.log('enabled'),
+    temp,
+    browser.getElementData('attribute', 'maxlength'),
+    async.console.log('attribute:maxlength'),
+    temp,
+    browser.getElementData('displayed'),
+    async.console.log('displayed'),
+    temp,
+    browser.getElementData('location'),
+    async.console.expand('location'),
+    browser.destroySession()
+    //async.console.log(),
+  ]);
+};
+
+fnc().call(null, null, complete, complete);
 
 
 
